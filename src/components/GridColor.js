c13e90d90aa8c157a4f922e3872acf1367585e18
sder12 <sara.cetto@gmail.com>
2023-04-06 17:04:56 +0200

    Scaffolding

---
+++ src/components/GridColor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SingleColor from './SingleColor';
+
+const GridColor = () => {
+    return (
+        <div>
+            <h3>form</h3>
+
+            <SingleColor />
+        </div>
+    )
+}
+
+export default GridColor
