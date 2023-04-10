b7286c6124d9d307d6dbba3ce67b25b7b9d5fc16
sder12 <sara.cetto@gmail.com>
2023-04-10 10:04:00 +0200

    Add function when click a color alert

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const SingleColor = (props) => {
+    const copyHex = () => {
+        alert(props.colorgradient)
+    }
+
     return (
         <div className='col-4 col-colorHex'
             style={{
                 backgroundColor: `${props.colorgradient}`
             }}>
-            {props.colorgradient}
+            <h5 onClick={copyHex} >
+                {props.colorgradient}
+            </h5>
         </div >
 
     )
